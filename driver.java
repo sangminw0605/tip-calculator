e41e31e473099c03981fd728e79e1d226a20f300
sangminw0605 <swoo17@jhu.edu>
2022-04-10 01:55:03 -0400

    one line

--- driver.java
+++ driver.java
@@ -1,5 +1,1 @@
-public class driver {
-    public static void main(String[] args) {
-        System.out.println("Tip: $" + Double.parseDouble(args[0]) * .15    + "\nTotal cost: $" + (Double.parseDouble(args[0]) * .15 + Double.parseDouble(args[0])));
-    }
-}
+public class driver { public static void main(String[] args) { System.out.println("Tip: $" + Double.parseDouble(args[0]) * .15    + "\nTotal cost: $" + (Double.parseDouble(args[0]) * .15 + Double.parseDouble(args[0])));}}
